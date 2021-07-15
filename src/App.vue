1ffc6f9b91bdfcff5ace02ff786e2c02c0b0a859
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-15 17:12:33 -0300

    feat: Added Vue Router in the project.

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,19 @@
 <template>
     <v-app>
-        <v-app-bar app color="green lighten-1" dark>
-            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <AppTopBar />
 
-            <v-toolbar-title>Campeonato Brasileiro 2020 ⚽</v-toolbar-title>
-        </v-app-bar>
-        <v-navigation-drawer v-model="drawer" absolute temporary>
-            <v-list nav dense>
-                <v-list-item-group v-model="group" active-class="green--text text--lighten-2">
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-home</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title>Início</v-list-item-title>
-                    </v-list-item>
-
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-soccer</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title>Classificação</v-list-item-title>
-                    </v-list-item>
-                </v-list-item-group>
-            </v-list>
-        </v-navigation-drawer>
         <v-main> <Home /> </v-main>
     </v-app>
 </template>
 
 <script>
-import Home from "./components/Home.vue";
+import Home from "./views/Home.vue";
+import AppTopBar from "./components/shared/AppTopBar";
 export default {
     name: "App",
-
     components: {
         Home,
+        AppTopBar,
     },
-
-    data: () => ({
-        drawer: false,
-        group: null,
-    }),
 };
 </script>
