46e02344ee766ee09117cabc537109d6cd5c169b
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-15 19:03:23 -0300

    feat: Added routes with router-link in the list icons

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
     <v-app>
         <AppTopBar />
-
-        <v-main> <Home /> </v-main>
+        <v-main> <router-view /> </v-main>
     </v-app>
 </template>
 
 <script>
-import Home from "./views/Home.vue";
 import AppTopBar from "./components/shared/AppTopBar";
 export default {
     name: "App",
     components: {
-        Home,
         AppTopBar,
     },
 };
 </script>
+
+<style scoped>
+.link-home {
+    color: white !important;
+    text-decoration: none;
+}
+</style>
