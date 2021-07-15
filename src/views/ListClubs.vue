da837b42db45db02c39f9c3a98fdc3c45d2fb4f4
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-15 19:00:48 -0300

    feat: Create component TableClubs.

---
+++ src/views/ListClubs.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-5">Classificação dos clubes</h2>
+        <TableClubs :clubes="listClubs" />
+    </v-container>
+</template>
+
+<script>
+import TableClubs from "../components/TableClubs";
+
+export default {
+    name: "ListClubs",
+    components: {
+        TableClubs,
+    },
+    data() {
+        return {
+            listClubs: [],
+        };
+    },
+    created() {
+        fetch("https://hackthon-decola.firebaseio.com/clubes-lista.json")
+            .then((response) => response.json())
+            .then((json) => {
+                this.listClubs = json;
+            });
+    },
+};
+</script>
+
+<style scoped></style>
